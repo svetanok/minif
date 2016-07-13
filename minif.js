67ee0dde9bab48d0cde1988c63ab3967bcc56f04
Svetlana Kavrugina <svetanok7920@gmail.com>
2016-07-14 00:05:55 +0300

    Minification implemented, comments added

--- minif.js
+++ minif.js
@@ -2,43 +2,84 @@
   * @param {Array} data – массив CSS классов
   */
 module.exports = function(data) {
+    /* алфавит для построения css-классов */
+    var letterAlph = 'abcdefghjiklmnopqrstuvwxyz';
 
-    var hash = []; /* вспомогательный хеш для полсчета частоты встречаемости */
-    var newHash = {};
-    var alAlph = 'abcdefghjiklmnopqrstuvwxyz';
-    CONST_LEN_ALPH = 26;
+    /* длина этого алфавита */
+    var CONST_LEN_ALPH = letterAlph.length;
 
-    function findFrequency(value, index)
+    /* ассоциативный вспомогательный массив, чтобы посчитать, сколько
+    раз встречается каждый класс в исходном файле;*/
+    var freqClassHash = {};
+
+    /* объект freqClassHash удобен, чтобы посчитать частоту встречаемости
+    класса, но не удобен для сортировки, поэтому ввожу freqClassArray,
+    который будет результатом преобразования freqClassHash в массив строк */
+    var freqClassArray = [];
+
+    /* объект, который вернет данный модуль, когда заполнит */
+    var newfreqClassHash = {};
+
+    /* функция генерирует новое имя класса по алгоритму, описанному в Readme */
+    function createNewClassName(_item, _index)
     {
-        /* этот варивнт считает число вхождения каждого класса в исходном
-        json-файле*/
-        /*hash[value] = (value in hash)?hash[value]+1:1;*/
+        var i = _index;
+        var k = i % CONST_LEN_ALPH;
 
-        if (hash.indexOf(value)==-1)
-            hash.push(value);
-    }
+        var str = letterAlph.charAt(k);
 
-    function createNewClassName(item, index)
-    {
-        var str='';
-
-        var k = index % CONST_LEN_ALPH;
-        var iteration = (index - k)/CONST_LEN_ALPH;
-
-        if (iteration>0)
+        while (i > k)
         {
-            for (j = 0; j < iteration; j++)
-            {
-                str=str+alAlph.charAt(j);
-            }
+            i = (i - k) / CONST_LEN_ALPH;
+            k = i % CONST_LEN_ALPH;
+            str = letterAlph.charAt(k) + str;
         }
-        str=str+alAlph.charAt(k);
-        newHash[item]=str;
+
+        return str;
     }
 
-    data.forEach(findFrequency);
-    hash.forEach(createNewClassName);
+    /* функция для сортировки двух строк вида "число%строка" */
+    function compareStrWithNum(_str1, _str2)
+    {
+        var a = parseInt(_str1.substring(0, _str1.indexOf('%')));
+        var b = parseInt(_str2.substring(0, _str2.indexOf('%')));
 
-    return newHash;
+        return (a < b) ? 1 : (a > b) ? -1 : 0;
+    }
+
+    /* считаю частоту встречаемости каждого класса в исходном файле */
+    data.forEach(function(_value, _index) {
+        freqClassHash[_value] = (_value in freqClassHash) ? freqClassHash[_value] + 1 : 1;
+
+    });
+
+    /*
+    преобразовываю каждый элемент массива freqClassHash вида:
+    {
+        класс: частота
+    }
+    в строку вида "частота%класс". В качестве разделителя использую "%",
+    т.к. этот символ не может встречаться в названии css класса по
+    спецификации */
+    var freqClassKeys = Object.keys(freqClassHash);
+
+    freqClassArray = freqClassKeys.map(function(_key)
+    {
+        return freqClassHash[_key] + "%" + _key;
+    });
+
+    /* сортирую массив freqClassArray по-убыванию. Сортировка происходит
+    по-убыванию числа в каждой строчки до "%" */
+    freqClassArray.sort(compareStrWithNum);
+
+    /* теперь каждый элемент вида "частота%класс" -> "класс" и генерируем
+    для него новое имя */
+    freqClassArray.forEach(function(item, index) {
+        var itemKey = item.substring(item.indexOf("%") + 1);
+
+        newfreqClassHash[itemKey] = createNewClassName(item, index);
+    });
+
+    return newfreqClassHash;
 };
 
